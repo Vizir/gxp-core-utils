aaef4146df5577a2fb0fdc8f827233fd1c607242
Guilherme Afonso <guilherme.afonso@vizir.com.br>
2022-01-20 15:55:25 -0300

    feat(get-web-uri): update env validation

--- src/getWebUri.ts
+++ src/getWebUri.ts
@@ -3,7 +3,7 @@ import { Storage } from "./storage/storage";
 import { CountriesUriType } from "./types";
 
 export const getWebUri = () => {
-  if (!process.env.MN_WEB_APP_URI) {
+  if (process.env.NODE_ENV === "production") {
     const country = Storage.getData(STORAGE_KEYS.COUNTRY).toUpperCase();
     return countriesUri[country as keyof CountriesUriType];
   }
